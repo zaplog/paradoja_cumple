5d068aa7ff5513b4c46b9ceba479f313bc4853bd
Gonzalo Martín Zapata López <gmzl1@gcloud.ua.es>
2024-12-26 16:08:14 +0100

    first commit

---
+++ paradojaCumple.py
@@ -0,0 +1,25 @@
+import random
+
+def salaCumple(nPersonas):
+    sala = []
+    for n in range(nPersonas):
+        cumple = random.randint(1,365)
+        sala.append(cumple)
+    
+    sala.sort()    
+    for n in range(len(sala)-1):
+        if sala[n] == sala[n+1]:
+            return True
+
+def main():
+    counter = 0
+    nPersonas = input("Cuantás personas habrá en la sala? :")
+    nPersonas = int(nPersonas)
+    for i in range(100):
+        if salaCumple(nPersonas):
+            counter = counter + 1
+    print("La probabilidad de que se repita el cumpleaños es de : "+ str(counter) + "%")
+    
+    
+if __name__ == "__main__":
+    main()
