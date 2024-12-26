7a503c93003021588e79fbd138add87d074db35e
Gonzalo Martín Zapata López <gmzl1@gcloud.ua.es>
2024-12-26 18:18:48 +0100

    BUG

--- paradojaCumple.py
+++ paradojaCumple.py
@@ -18,7 +18,8 @@ def main():
     for i in range(100):
         if salaCumple(nPersonas):
             counter = counter + 1
-    print("La probabilidad de que se repita el cumpleaños es de : "+ str(counter) + "%")
+    #print("La probabilidad de que se repita el cumpleaños es de : "+ str(counter) + "%")
+    print("BUG APLICACIÓN ROTA")
     
     
 if __name__ == "__main__":
